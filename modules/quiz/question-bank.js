e4ee2b26ba76b66cb1989dcfdcc50c4494cc887c
sandeep80 <sandeep@ptotem.com>
2014-12-02 19:22:54 +0530

    Slide_id intergrated

--- modules/quiz/question-bank.js
+++ modules/quiz/question-bank.js
@@ -20,7 +20,9 @@ questionbank.questions = [
 
         optd: "Get to the point.",
         optdcorrect: "true",
-        optdpoints: "113"
+        optdpoints: "113",
+
+        slide_id: 321
     },
     {
         statement: "It is about the insurance policy you offer. I wanted to get an idea of how and what it is!",
@@ -41,7 +43,9 @@ questionbank.questions = [
 
         optd: "Okay.",
         optdcorrect: "true",
-        optdpoints: "113"
+        optdpoints: "113",
+
+        slide_id: 321
     },
     {
         statement: "Well, what benefits do I have in this policy?",
@@ -62,7 +66,9 @@ questionbank.questions = [
 
         optd: "Option 3D",
         optdcorrect: "true",
-        optdpoints: "113"
+        optdpoints: "113",
+
+        slide_id: 321
     },
     {
         statement: "Statement 4",
@@ -83,7 +89,9 @@ questionbank.questions = [
 
         optd: "Option 4D",
         optdcorrect: "true",
-        optdpoints: "113"
+        optdpoints: "113",
+
+        slide_id: 321
     },
     {
         statement: "Statement 5",
@@ -104,7 +112,9 @@ questionbank.questions = [
 
         optd: "Option 5D",
         optdcorrect: "true",
-        optdpoints: "113"
+        optdpoints: "113",
+
+        slide_id: 321
     },
     {
         statement: "Statement 6",
@@ -125,7 +135,9 @@ questionbank.questions = [
 
         optd: "Option 6D",
         optdcorrect: "true",
-        optdpoints: "113"
+        optdpoints: "113",
+
+        slide_id: 321
     },
     {
         statement: "Statement 7",
@@ -146,7 +158,9 @@ questionbank.questions = [
 
         optd: "Option 7D",
         optdcorrect: "true",
-        optdpoints: "113"
+        optdpoints: "113",
+
+        slide_id: 321
     },
     {
         statement: "Statement 8",
@@ -167,7 +181,9 @@ questionbank.questions = [
 
         optd: "Option 8D",
         optdcorrect: "true",
-        optdpoints: "113"
+        optdpoints: "113",
+
+        slide_id: 321
     },
     {
         statement: "Statement 9",
@@ -188,7 +204,9 @@ questionbank.questions = [
 
         optd: "Option 9D",
         optdcorrect: "true",
-        optdpoints: "113"
+        optdpoints: "113",
+
+        slide_id: 321
     },
     {
         statement: "Statement 10",
@@ -209,111 +227,11 @@ questionbank.questions = [
 
         optd: "Option 10D",
         optdcorrect: "true",
-        optdpoints: "113"
+        optdpoints: "113",
+
+        slide_id: 321
     }
 
 ]
 
-//questionbank.questions = [
-//    {
-//        statement: "Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 Statement 1 ",
-//        weight: 1,
-//        options: [
-//            {option: 1, name: "Option 1A Option 1A Option 1A Option 1A Option 1A Option 1A Option 1A Option 1A Option 1A ", correct: true, points: "014"},
-//            {option: 2, name: "Option 1B Option 1B Option 1B Option 1B Option 1B Option 1B Option 1B Option 1B Option 1B ", correct: true, points: "401"},
-//            {option: 3, name: "Option 1C Option 1C Option 1C Option 1C Option 1C ", correct: true, points: "050"},
-//            {option: 4, name: "Option 1D Option 1D Option 1D Option 1D Option 1D ", correct: true, points: "113"}
-//        ]
-//    },
-//    {
-//        statement: "Statement 2",
-//        weight: 1,
-//        options: [
-//            {option: 1, name: "Option 2A", correct: true, points: "014"},
-//            {option: 2, name: "Option 2B", correct: true, points: "401"},
-//            {option: 3, name: "Option 2C", correct: true, points: "050"},
-//            {option: 4, name: "Option 2D", correct: true, points: "113"}
-//        ]
-//    },
-//    {
-//        statement: "Statement 3",
-//        weight: 1,
-//        options: [
-//            {option: 1, name: "Option 3A", correct: true, points: "014"},
-//            {option: 2, name: "Option 3B", correct: true, points: "401"},
-//            {option: 3, name: "Option 3C", correct: true, points: "050"},
-//            {option: 4, name: "Option 3D", correct: true, points: "113"}
-//        ]
-//    },
-//    {
-//        statement: "Statement 4",
-//        weight: 1,
-//        options: [
-//            {option: 1, name: "Option 4A", correct: true, points: "014"},
-//            {option: 2, name: "Option 4B", correct: true, points: "401"},
-//            {option: 3, name: "Option 4C", correct: true, points: "050"},
-//            {option: 4, name: "Option 4D", correct: true, points: "113"}
-//        ]
-//    },
-//    {
-//        statement: "Statement 5",
-//        weight: 1,
-//        options: [
-//            {option: 1, name: "Option 5A", correct: true, points: "014"},
-//            {option: 2, name: "Option 5B", correct: true, points: "401"},
-//            {option: 3, name: "Option 5C", correct: true, points: "050"},
-//            {option: 4, name: "Option 5D", correct: true, points: "113"}
-//        ]
-//    },
-//    {
-//        statement: "Statement 6",
-//        weight: 1,
-//        options: [
-//            {option: 1, name: "Option 6A", correct: true, points: "014"},
-//            {option: 2, name: "Option 6B", correct: true, points: "401"},
-//            {option: 3, name: "Option 6C", correct: true, points: "050"},
-//            {option: 4, name: "Option 6D", correct: true, points: "113"}
-//        ]
-//    },
-//    {
-//        statement: "Statement 7",
-//        weight: 1,
-//        options: [
-//            {option: 1, name: "Option 7A", correct: true, points: "014"},
-//            {option: 2, name: "Option 7B", correct: true, points: "401"},
-//            {option: 3, name: "Option 7C", correct: true, points: "050"},
-//            {option: 4, name: "Option 7D", correct: true, points: "113"}
-//        ]
-//    },
-//    {
-//        statement: "Statement 8",
-//        weight: 1,
-//        options: [
-//            {option: 1, name: "Option 8A", correct: true, points: "014"},
-//            {option: 2, name: "Option 8B", correct: true, points: "401"},
-//            {option: 3, name: "Option 8C", correct: true, points: "050"},
-//            {option: 4, name: "Option 8D", correct: true, points: "113"}
-//        ]
-//    },
-//    {
-//        statement: "Statement 9",
-//        weight: 1,
-//        options: [
-//            {option: 1, name: "Option 9A", correct: true, points: "014"},
-//            {option: 2, name: "Option 9B", correct: true, points: "401"},
-//            {option: 3, name: "Option 9C", correct: true, points: "050"},
-//            {option: 4, name: "Option 9D", correct: true, points: "113"}
-//        ]
-//    },
-//    {
-//        statement: "Statement 10",
-//        weight: 1,
-//        options: [
-//            {option: 1, name: "Option 10A", correct: true, points: "014"},
-//            {option: 2, name: "Option 10B", correct: true, points: "401"},
-//            {option: 3, name: "Option 10C", correct: true, points: "050"},
-//            {option: 4, name: "Option 10D", correct: true, points: "113"}
-//        ]
-//    }
-//
-//];
+questionbank.questions = parent.getQuestionFromBank(parent.currentIntergratedGame);
